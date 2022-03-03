4e9c3a06fc23e927ac8df9cd631c8953379e65aa
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-03 15:54:32 +0100

    solved error on variables name

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
-const name = prompt("Insert your name");
+const personName = prompt("Insert your name");
 const surname = prompt("Insert your surname");
 const color = prompt("Insert your fav color");
 //print password in the console
-console.log(`${name}${surname}${color}21`);
+console.log(`${personName}${surname}${color}21`);
 //write password in the html
-document.getElementById("password").innerText = `${name}${surname}${color}21`;
+document.getElementById("password").innerText = `${personName}${surname}${color}21`;
